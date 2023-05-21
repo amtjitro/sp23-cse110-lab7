fa71fcf556b6edbbc8fb8f1d1e78e8c69e27f4bc
amtjitro <60487970+amtjitro@users.noreply.github.com>
2023-05-20 19:37:04 -0700

    finished both parts

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -53,6 +53,24 @@ function initializeServiceWorker() {
   //            log that it was successful.
   // B5. TODO - In the event that the service worker registration fails, console
   //            log that it has failed.
+
+  if ("serviceWorker" in navigator) {
+
+    try {
+
+      window.addEventListener('load', swFunc);
+
+      function swFunc() {
+  
+      }
+      const registration = navigator.serviceWorker.register("./sw.js");
+      console.log("successful registration of service worker");
+    } catch (error) {
+      console.error(`Registration failed with ${error}`);
+    }
+
+  }
+
   // STEPS B6 ONWARDS WILL BE IN /sw.js
 }
 
@@ -68,16 +86,24 @@ async function getRecipes() {
   // EXPOSE - START (All expose numbers start with A)
   // A1. TODO - Check local storage to see if there are any recipes.
   //            If there are recipes, return them.
+
+  let localInputRecipes = localStorage.getItem('recipes');
+  if (localInputRecipes) {
+    return JSON.parse(localInputRecipes);
+  }
+
   /**************************/
   // The rest of this method will be concerned with requesting the recipes
   // from the network
   // A2. TODO - Create an empty array to hold the recipes that you will fetch
+  let recipesArr = [];
   // A3. TODO - Return a new Promise. If you are unfamiliar with promises, MDN
   //            has a great article on them. A promise takes one parameter - A
   //            function (we call these callback functions). That function will
   //            take two parameters - resolve, and reject. These are functions
   //            you can call to either resolve the Promise or Reject it.
   /**************************/
+
   // A4-A11 will all be *inside* the callback function we passed to the Promise
   // we're returning
   /**************************/
@@ -100,6 +126,24 @@ async function getRecipes() {
   //            resolve() method.
   // A10. TODO - Log any errors from catch using console.error
   // A11. TODO - Pass any errors to the Promise's reject() function
+
+  return new Promise(async (resolve, reject) => {
+    for (const url of RECIPE_URLS) {
+      try {
+        var fetchedUrl = await fetch (url);
+        var urlJSON = await fetchedUrl.json();
+        recipesArr.push(urlJSON);
+        if (recipesArr.length == RECIPE_URLS.length) {
+          saveRecipesToStorage(recipesArr);
+          resolve(recipesArr);
+        }
+      } catch (err) {
+        console.error(err);
+        reject(err);
+      }
+    }
+  });
+
 }
 
 /**
@@ -126,4 +170,4 @@ function addRecipesToDocument(recipes) {
     recipeCard.data = recipe;
     main.append(recipeCard);
   });
-}
+}  
